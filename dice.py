829b0e3acac4aa4e076083764fd9ed11a84590bf
Sean <77973833+SeanC451@users.noreply.github.com>
2021-06-15 16:25:33 -0400

    Update dice.py
    Added try/except to end program neatly if user enters strings instead of numbers.

--- dice.py
+++ dice.py
@@ -8,20 +8,23 @@ import os
 import random
 
 def roll_dice(x, y, z):
-    count = 1                                                  # Set counter at 1 for first iteration.
+    count = 1                                                   # Set counter at 1 for first iteration.
 
-    rolled_num = 0                                             # Initialize number rolled counters.
+    rolled_num = 0                                              # Initialize number rolled counters.
     roll_total = 0
 
-    dice = int(x)                                              # Convert strings to integers for math operations.
-    sides = int(y)
-    addon = int(z)
+    try:
+        dice = int(x)                                           # Convert strings to integers for math operations.
+        sides = int(y)                                          # If user entered a string, break and end program.
+        addon = int(z)
+    except:
+        return
 
-    while(count <= dice):                                      # Loop to iterate for each number of dice user wishes to roll.
-        rolled_num = random.randint(1, sides)                  # Generate random number from 1 to sides of dice.
-        # print(f"Die #{count} is {rolled_num}")               # Make sure of randomness. Uncomment to debug.
-        roll_total += rolled_num                               # Accumulate value
-        count += 1                                             # Increment counter.
+    while(count <= dice):                                       # Loop to iterate for each number of dice user wishes to roll.
+        rolled_num = random.randint(1, sides)                   # Generate random number from 1 to sides of dice.
+        # print(f"Die #{count} is {rolled_num}")                # Make sure of randomness. Uncomment to debug.
+        roll_total += rolled_num                                # Accumulate value
+        count += 1                                              # Increment counter.
 
     return roll_total + addon
 
@@ -31,6 +34,9 @@ numDice = input("Enter number of dice: ")                       # Input user def
 numSides = input("Enter number of sides: ")
 numAddon = input("Enter add-on: ")
 
-roll = roll_dice(numDice, numSides, numAddon)                   # Pass variables to roll_dice function.
+roll = roll_dice(numDice, numSides, numAddon)                    # Pass variables to roll_dice function.
 
-print(f"Your roll of {numDice}d{numSides}+{numAddon} is {roll}.")
+if roll == None:
+    print('\nPlease enter only numbers!\n')
+else:
+    print(f"\nYour roll of {numDice}d{numSides}+{numAddon} is {roll}.\n")
